b139cc1820669bc85ea80954fd34683b9b80c8d1
NarendraReddy <narendra.nya@gmail.com>
2024-04-27 18:34:15 -0700

    H

--- 02-DU.sh
+++ 02-DU.sh
@@ -6,6 +6,10 @@ THRESHOLD=6
 while IFS= read -r line
 do 
     DISK_USAGE_LIMIT=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    echo $DISK_USAGE_LIMIT
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
     
+    if [ $DISK_USAGE_LIMIT -ge THRESHOLD ];
+    then
+        echo "Disk usage for $FOLDER is exceeds $THRESHOLD,current usage:$DISK_USAGE_LIMIT"
+    fi
 done <<<$DISK_USAGE
