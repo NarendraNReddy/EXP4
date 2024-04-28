02a71b5b93e18c898930206f31c552f303e63a29
NarendraReddy <narendra.nya@gmail.com>
2024-04-27 18:31:57 -0700

    H

--- 02-DU.sh
+++ 02-DU.sh
@@ -6,6 +6,6 @@ THRESHOLD=6
 while IFS= read -r line
 do 
     DISK_USAGE_LIMIT=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    echo "$DISK_USAGE_LIMIT \n"
+    echo $DISK_USAGE_LIMIT
     
 done <<<$DISK_USAGE
